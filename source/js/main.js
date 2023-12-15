f920ca3b89468d415a49c51bcc6a878f2fc4f096
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2023-12-15 13:50:48 +0400

    fix: удаляет неиспользуемые файлы модалки

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {mobileVhFix} from './utils/mobile-vh-fix.js';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
@@ -30,7 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     uploadFile();
     uploadImageDrop();
     const select = new CustomSelect();
