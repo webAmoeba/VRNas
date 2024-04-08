32a587cf228e0110eb391d4ee837f3c668274a6b
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-04-08 04:12:51 +0400

    Update 2024-04-08T00:12:35.446Z

--- source/js/modules/header/init-header.js
+++
@@ -1,9 +0,0 @@
-import {Burger} from './burger';
-import {StickyHeader} from './sticky-header';
-
-export const initHeader = () => {
-  const burger = new Burger();
-  burger.init();
-  const stickyHeader = new StickyHeader();
-  stickyHeader.init();
-};
